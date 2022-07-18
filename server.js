abe31cd96ab816d61442a7fc885b617dc666be10
Dylan Mitchley <[dylanmitchley1994@gmail.com]>
2022-07-18 16:55:34 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+import dotenv from "dotenv";
+dotenv.config();
+// importing the routes
+const routes = require("./routes/index");
+const helmet = require("helmet");
+const path = require("path");
+
+// initializing express
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+// HTTP request middleware logger
+app.use(morgan("dev"));
+
+// accessing the routes from the routes folder with /api
+app.use("/api", routes);
+
+// defining the port
+const PORT = process.env.PORT || 8080;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`
+  )
+);
